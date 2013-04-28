e1033921150ef63f67eb17b278ed3e2629b845a6
Simon Baker <skbarker@me.com>
2013-04-28 18:14:48 +0100

    modified add rating to use string for DOM update and fully chained DOM modification - thanks Mark Morely

--- app.js
+++ app.js
@@ -55,19 +55,15 @@ Rating provided through the Open Movie Database API - http://www.omdbapi.com.
     }
 
     function addRating(sender){
-      var url = "http://www.omdbapi.com/?t=" + $(sender).attr("title");
-
-      var that = sender;
+      // make 'that' a jquery object straight away for use here so no need for multiple calls to jquery
+      var $that = $(sender),
+      url = "http://www.omdbapi.com/?t=" + $that.attr("title");
 
       $.getJSON(url, function(data) {
-
-        ratingText = getRating(data);
-
-        var rating = $('<p>', { class: "imdbRating", text: "IMDB: "});
-        $(rating).append(ratingText);
-
-        $(that).closest(".product").find("img.ajaxloader").replaceWith(rating);
-        $(that).addClass("imdbRated");
+        // no need here for rating to be jquery either.
+        var rating = '<p class="imdbRating">IMDB: ' + getRating(data) + '</p>';
+        // chain this bad boy all the way
+        $that.closest(".product").find("img.ajaxloader").replaceWith(rating).addClass("imdbRated");
       });
     }
   }
@@ -88,8 +84,7 @@ Rating provided through the Open Movie Database API - http://www.omdbapi.com.
   }
 
   function getRating(data){
-    ratingText = (data.Response !== "False") 
-      ? data.imdbRating : "Not Found";
+    ratingText = (data.Response !== "False") ? data.imdbRating : "Not Found";
     return '<span class="itempropIMDBrating">'+ratingText+'</span>';
   }
 
